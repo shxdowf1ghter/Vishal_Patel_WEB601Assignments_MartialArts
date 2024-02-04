2c3a639bc563514bd1f4050c7dc979cdc61fec7e
Vishal N Patel <133668085+shxdowf1ghter@users.noreply.github.com>
2024-02-03 19:49:18 -0500

    deleted old files

--- V_Patel_MartialArts/src/app/defense-type.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DefenseTypePipe } from './defense-type.pipe';
-
-describe('DefenseTypePipe', () => {
-  it('create an instance', () => {
-    const pipe = new DefenseTypePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- V_Patel_MartialArts/src/app/martial-photos.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MartialPhotosDirective } from './martial-photos.directive';
-
-describe('MartialPhotosDirective', () => {
-  it('should create an instance', () => {
-    const directive = new MartialPhotosDirective();
-    expect(directive).toBeTruthy();
-  });
-});
