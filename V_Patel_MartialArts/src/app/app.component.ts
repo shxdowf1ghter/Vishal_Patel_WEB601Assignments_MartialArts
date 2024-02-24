add7c22b20da2a85c34afd0303fd3a95a180f568
Vishal N Patel <133668085+shxdowf1ghter@users.noreply.github.com>
2024-02-24 17:40:42 -0500

    upload final

---
+++ V_Patel_MartialArts/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  template: '<app-content-card></app-content-card>',
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+})
+export class AppComponent {
+  title = 'V_Patel_Martialarts';
+}
---
+++ V_Patel_MartialArts/src/app/content-card/content-card.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+import { Content } from '../helper-files/content-interface';
+
+@Component({
+  selector: 'app-content-card',
+  templateUrl: './content-card.component.html',
+  styleUrls: ['./content-card.component.scss'],
+})
+export class ContentCardComponent {
+  @Input()
+  content!: Content;
+
+  constructor() {}
+
+  handleClick(contentLog: Content): void {
+    console.log(`ID: ${contentLog.id}, TITLE: ${contentLog.title}`);
+  }
+}
