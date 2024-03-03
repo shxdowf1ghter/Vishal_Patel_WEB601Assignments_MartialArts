0b027c2ca47efd847cfbba09a7fc79d92be2e298
Vishal N Patel <133668085+shxdowf1ghter@users.noreply.github.com>
2024-03-02 20:42:44 -0500

    old files deleted

--- V_Patel_MartialArts/src/app/filter-content.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilterContentPipe } from './filter-content.pipe';
-
-describe('FilterContentPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterContentPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
